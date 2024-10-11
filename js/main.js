df0d720affe250d781fbe6ee769bdc6c7a0a4f97
ClevertonRoch <nativawebsite.brasil@gmail.com>
2024-10-11 10:11:41 -0300

    projeto ebac motors

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    $('form').on('submit', function (e) {
+        e.preventDefault()
+    })
+
+    $('#carousel-imagens').slick({
+        autoplay: true
+    })
+
+    $('.menu-hamburguer').click(function () {
+        $('nav').slideToggle()
+    })
+
+    $('#telefone').mask('(00) 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true,
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+            },
+        },
+        messages: {
+            nome: "Por favor, informe seu nome"
+        },
+        submitHandler: function (form) {
+            console.log(form)
+        },
+        invalidHandler: function (event, validator) {
+            let campoDoForm = validator.numberOfInvalids();
+            console.log(campoDoForm)
+        }
+    })
+
+    $('.lista-veiculos button').click(function () {
+        const destino = $('#contato')
+        const nomeVeiculo = $(this).parent().find('h3').text()
+
+
+        $('#veiculo-interesse').val(nomeVeiculo.replace('Carro', '').trim())
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+})
