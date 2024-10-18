cc8adf68782d5bfd69704c70e282b84ff4a03265
ClevertonRoch <nativawebsite.brasil@gmail.com>
2024-10-18 11:25:56 -0300

    Enable popover

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $('document').ready(function () {
 
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
     $('#tel').mask("(00) 00000-0000")
 
     $("form").on('submit', function (e) {
