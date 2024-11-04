d4484e2ef5092fff6c8369112e3bd33cffea40ac
ClevertonRoch <nativawebsite.brasil@gmail.com>
2024-11-04 10:40:25 -0300

    adicionando outras tarefas na eportação

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,36 @@
-const GulpClient = require('gulp');
-const { src, dest } = require('gulp');
+const { src, dest, series, watch } = require('gulp');
 const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+
 
 function compilesass() {
     return src('./source/styles/main.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }))
+        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.write('./maps'))
         .pipe(dest('./build/styles'));
 
 }
 
-function minificaimage(cb) {
+function compimage(cb) {
     return src('./source/images/*')
         .pipe(imagemin())
         .pipe(dest('./build/images'))
     cb();
 }
 
-function minificascript() {
+function compscript() {
     return src("./source/scripts/*js")
         .pipe(uglify())
         .pipe(dest('./build/scripts'))
 }
 
-
-exports.compile = minificascript;
+exports.default = function () {
+    watch('./source/styles/*.scss', { ignoreInitial: false }, series(compilesass));
+    watch('./source/scripts/*.js', { ignoreInitial: false }, series(compscript));
+    watch('./source/images/*', { ignoreInitial: false }, series(compimage));
+};
