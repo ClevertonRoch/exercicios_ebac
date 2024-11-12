2d090364da62a7c0d82c91485e2c9bc85982cb0e
ClevertonRoch <nativawebsite.brasil@gmail.com>
2024-11-12 00:28:35 -0300

    commit init

---
+++ src/scripts/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('formId').addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        let numMax = document.getElementById('numMax').value;
+        numMax = parseInt(numMax);
+
+        let numRandom = Math.random() * numMax;
+        numRandom = Math.floor(numRandom + 1);
+
+        if (numRandom) {
+            document.getElementById('result').innerText = "Número é: " + numRandom; 
+        }
+
+    })
+})
