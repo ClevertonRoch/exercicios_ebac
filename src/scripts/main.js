28eac410a93818e644ce06645480db69f45a376c
ClevertonRoch <nativawebsite.brasil@gmail.com>
2024-11-13 09:33:25 -0300

    adicionando função de sorteio de numero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,45 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('formId').addEventListener('submit', (e) => {
+        e.preventDefault();
 
-function somar(a, b) {
-    return a + b;
-}
+        let numMax = document.getElementById('numMax').value;
+        numMax = parseInt(numMax);
 
-function ePar(numero) {
-    return numero % 2 === 0;
-}
+        let numRandom = Math.random() * numMax;
+        numRandom = Math.floor(numRandom + 1);
 
-function fatorial(numero) {
-    if (numero === 0 || numero === 1) {
-        return 1;
-    } else {
-        return numero * fatorial(numero - 1);
-    }
-}
-
-function toUpperCase(str) {
-    return str.toUpperCase();
-}
-
-function ePalindromo(str) {
-    const tamanho = str.length;
-    for (let i = 0; i < tamanho / 2; i++) {
-        if (str[i] !== str[tamanho - i - 1]) {
-            return false;
+        if (numRandom) {
+            document.getElementById('result').innerText = "Número é: " + numRandom;
         }
-    }
-    return true;
-}
 
-function getDataFormatada() {
-    const agora = new Date();
-    const dia = String(agora.getDate()).padStart(2, '0');
-    const mes = String(agora.getMonth() + 1).padStart(2, '0');
-    const ano = agora.getFullYear();
-    return `${dia}/${mes}/${ano}`;
-}
-
-console.log("A soma de 2 e 3 é:", somar(2, 3));
-console.log("O número 4 é par?", ePar(4));
-console.log("O fatorial de 5 é:", fatorial(5));
-console.log("A string 'olá' em maiúsculas é:", toUpperCase('olá'));
-console.log("A string 'radar' é um palíndromo?", ePalindromo('radar'));
-console.log("A data atual formatada é:", getDataFormatada());
+    })
+})
